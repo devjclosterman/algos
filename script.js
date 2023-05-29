55b2a9e231771df3d846c80428693d9c8349e804
Jason Closterman <devjclosterman@gmail.com>
2023-05-29 15:58:50 -0700

    finished binary

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('Hello World')
+// console.log('Hello World')
 
 const binarySearch = (arr, target) => {
     let left = 0;
@@ -8,8 +8,26 @@ const binarySearch = (arr, target) => {
         let mid = Math.floor((left + right) / 2);
    console.log(mid)
         //Check if the middle element is the target
-
+if(arr[mid] === target) {
+    return mid; //Target found, return the index
+} else if(arr[mid] < target) {
+    left = mid + 1; //Target is greater, serach the right half
+} else {
+    right = mid - 1; //Target is smaller, search the left half
+}
     }
+    return -1; //Target not found
+} 
+
+console.log(binarySearch()) // Enter your params here
+//Example usage:
+const sortedArray = [1, 3, 5, 7, 9, 11, 13, 15];
+const target = 9;
+const resultIndex = binarySearch(sortedArray, target);
+
+if (resultIndex !== -1) {
+    console.log(`Target ${target} found at index ${resultIndex}`);
+  } else {
+    console.log(`Target ${target} not found in the array`);
 }
 
-console.log(binarySearch())
