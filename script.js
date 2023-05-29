aa16498b1fdd1f542c1d78333a145d810088b8fc
Jason Closterman <devjclosterman@gmail.com>
2023-05-29 15:15:36 -0700

    binarySearch

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('Hello World')
+
+const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+   console.log(mid)
+        //Check if the middle element is the target
+
+    }
+}
+
+console.log(binarySearch())
